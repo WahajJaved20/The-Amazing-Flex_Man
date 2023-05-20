fd92d0920cb20d3a237f0f86f5e963f064d5a73c
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-05-20 17:27:21 +0500

    summary working :D

--- background.js
+++ background.js
@@ -1,55 +1,19 @@
-// chrome.tabs.onUpdated.addListener(function(tabId,changeInfo, tab) {
-//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//         var currentTab = tabs[0];
-//         var url = currentTab.url;
-//         if(url && url.includes("flexstudent.nu.edu.pk/Student/StudentMarks?semid")){
-//             const queryParameters = url.split("?")[1];
-//             const urlParameters = new URLSearchParams(queryParameters);
-//             console.log(urlParameters);
-//             if (changeInfo.status === 'complete') {
-//                 chrome.scripting.executeScript(tabId, { code: 'document.documentElement.outerHTML' }, function (result) {
-//                   if (chrome.runtime.lastError) {
-//                     console.error(chrome.runtime.lastError);
-//                   } else {
-//                     var sourceCode = result[0];
-//                     console.log('Page Source Code:', sourceCode);
-//                   }
-//                 });
-//               }
-//             // chrome.tabs.sendMessage(tabId, {
-//             //     type:"NEW",
-//             //     semesterId: urlParameters.get("semid"),
-//             // });
-//         }
-       
-//     });
-    
-// });
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//   if (changeInfo.status === 'complete') {
-//     chrome.scripting.executeScript(tabId, { code: 'document.documentElement.outerHTML' }, function (result) {
-//       if (chrome.runtime.lastError) {
-//         console.error(chrome.runtime.lastError);
-//       } else {
-//         var sourceCode = result[0];
-//         console.log('Page Source Code:', sourceCode);
-//       }
-//     });
-//   }
-// });
-
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: tab.id },
-        files: ["contentScript.js"],
-      },
-      () => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError);
-        }
-      }
-    );
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "getHTMLCode") {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTab = tabs[0];
+        console.log(activeTab);
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: activeTab.id },
+            func: () => document.documentElement.outerHTML,
+          },
+          (result) => {
+            const htmlCode = result[0].result;
+            chrome.runtime.sendMessage({ action: "htmlCode", data: htmlCode });
+          }
+        );
+      });
+    }
   });
-  
   
---
+++ popup.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var getHtmlButton = document.getElementById("getHtmlButton");
+  getHtmlButton.textContent = "Calculate Summary :D";
+  getHtmlButton.addEventListener("click", function() {
+    chrome.runtime.sendMessage({ action: "getHTMLCode" });
+  });
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === "htmlCode") {
+    var htmlCode = message.data;
+    var tables = extractTables(htmlCode);
+    var selector = ".nav-link.m-tabs__link"
+    const extractedText = extractTextFromElements(htmlCode, selector);
+    const results = makeResultArrays(tables, extractedText);
+    populateTable(results, extractedText)
+    }
+});
+function populateTable(results, text) {
+  var tableBody = document.querySelector("#marksTable tbody");
+    for (var i = 0; i < results.length; i++) {
+      var row = document.createElement("tr");
+      for (var j = 0; j < 3; j++) {
+        var cell = document.createElement("td");
+        cell.textContent = results[i][j];
+        row.appendChild(cell);
+      }
+      tableBody.appendChild(row);
+  };
+}
+function extractTables(htmlCode) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlCode, "text/html");
+  const tables = doc.querySelectorAll("table");
+  
+  const tableData = [];
+  
+  tables.forEach(table => {
+    const rows = table.rows;
+    const tableRows = [];
+    
+    for (let i = 0; i < rows.length; i++) {
+      const rowData = [];
+      const cells = rows[i].cells;
+      
+      for (let j = 0; j < cells.length; j++) {
+        const cellData = cells[j].innerText.trim();
+        rowData.push(cellData);
+      }
+      
+      tableRows.push(rowData);
+    }
+    
+    tableData.push(tableRows);
+  });
+  
+  return tableData;
+}
+function extractTextFromElements(htmlCode, selector) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlCode, "text/html");
+  const elements = doc.querySelectorAll(selector);
+  
+  const textData = [];
+  
+  elements.forEach(element => {
+    const text = element.innerText.trim();
+    textData.push(text);
+  });
+  
+  return textData;
+}
+function makeResultArrays(tables, text) {
+  const resultArrays = [];
+  let courseIndex = 0;
+  let totalMarks = parseFloat("0");
+  let obtainedMarks = parseFloat("0");
+  for (let index = 0; index < tables.length; index++) {
+    const element = tables[index];
+    const finalElement = element[element.length - 1];
+    if (finalElement[0] === "Total Marks") {
+      resultArrays.push([text[courseIndex], totalMarks.toFixed(2), obtainedMarks.toFixed(2)]);
+      totalMarks = 0;
+      obtainedMarks = 0;
+      courseIndex++;
+    }else{
+      totalMarks += parseFloat(finalElement[1]);
+      obtainedMarks += parseFloat(finalElement[2]);
+    }
+    
+  }
+  return resultArrays;
+}
